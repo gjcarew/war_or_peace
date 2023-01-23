6cc8d156e15ec36abec6a3564702e0ab3819d636
Gavin Carew <gjcarew@gmail.com>
2023-01-23 11:03:00 -0800

    Revise file structure for testing

--- tests/test_card/test_card.py
+++ tests/test_card/test_card.py
@@ -1,12 +1,13 @@
 import pytest
-from card import Card
+from src.card import Card
 
 @pytest.fixture
 def card():
-    #Returns an queen of diamonds
+    '''Returns n queen of diamonds'''
     return Card('diamond', 'Queen', 12)
 
 def test_attributes(card):
+    ''' Tests that a card has attributes '''
     assert card.suit == 'diamond'
     assert card.value == 'Queen'
     assert card.rank == 12
